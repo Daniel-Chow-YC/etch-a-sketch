14b79e25f024e5b573cb361f9c69d7cbce5951d0
danielchowyc <daniellchow@gmail.com>
2020-07-15 20:51:29 +0100

    added a clear button

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Project: Etch-a-Sketch
 
 - In this project I'm going to build a browser version of something between a sketchpad and an Etch-A-Sketch.
+
+Note:
+- result of ``.querySelector()`` is not live, so when we dynamically add an element that matches a selector, the collection won’t update.
+- document.getElementsByClassName() is an HTMLCollection, and is live.
--- main.js
+++ main.js
@@ -1,20 +1,41 @@
 const container = document.querySelector(".container")
-
+// let square = document.querySelectorAll(".square");
+let square = document.getElementsByClassName("square");
 let div = document.createElement("div");
 div.classList.add("square");
 
 // creates 2500 divs (squares)
-for (let i = 0; i < 2500; i++) {
-    container.appendChild(div.cloneNode(true));
+function createGrid() {
+    for (let i = 0; i < 2500; i++) {
+        container.appendChild(div.cloneNode(true));
+    }
 }
-////
+createGrid();
 
-let square = document.querySelectorAll(".square");
-
-for (let i = 0 ; i < square.length; i++) {
-    square[i].onmouseover=function(){square[i].classList.add("colour")};
+function clearGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
 }
+
+// adds black colour when mouse moves over square
+function makeBlack() {
+    for (let i = 0 ; i < square.length; i++) {
+        square[i].onmouseover=function(){square[i].classList.add("colour")};
+    }
+}
+makeBlack();
 //// This is another way to write this for loop
 // for (let i in square) {
 //     square[i].onmouseover=function(){square[i].classList.add("colour")};
 // }
+
+// clear button -- returns all squares to white colour
+function reset() {
+    clearGrid();
+    createGrid();
+    makeBlack();
+}
+    
+// // create a 25 by 25 grid
+// function grid25() {}
