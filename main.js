82b62e88da48d7198a9b8df592f2b82272ad18b3
danielchowyc <daniellchow@gmail.com>
2020-07-15 21:35:34 +0100

    added more buttons

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-const container = document.querySelector(".container")
+const container = document.querySelector(".container");
 // let square = document.querySelectorAll(".square");
 let square = document.getElementsByClassName("square");
-let div = document.createElement("div");
-div.classList.add("square");
+let newSquare = document.createElement("div");
+newSquare.classList.add("square");
 
-// creates 2500 divs (squares)
-function createGrid() {
-    for (let i = 0; i < 2500; i++) {
-        container.appendChild(div.cloneNode(true));
+// creates 25x25 divs (squares)
+function createGrid(gridSize=25) {
+    for (let i = 0; i < gridSize*gridSize; i++) {
+        container.appendChild(newSquare.cloneNode(true));
     }
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 }
 createGrid();
 
@@ -30,12 +31,23 @@ makeBlack();
 //     square[i].onmouseover=function(){square[i].classList.add("colour")};
 // }
 
-// clear button -- returns all squares to white colour
+// reset button -- returns all squares to white colour
 function reset() {
     clearGrid();
     createGrid();
     makeBlack();
 }
     
-// // create a 25 by 25 grid
-// function grid25() {}
+// create a 50 by 50 grid
+function grid50() {
+    clearGrid();
+    createGrid(50);
+    makeBlack();
+}
+
+// create a 75 by 75 grid
+function grid75() {
+    clearGrid();
+    createGrid(75);
+    makeBlack();
+}
